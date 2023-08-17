4c4ab8a1ea57b1b938f1e624b2d1697f81b33143
Dennis Gilli <dennis.gilli@acribis.ch>
2023-08-17 21:15:52 +0200

    feat(zsh): add new list_scopes function to list scopes from commit history
    The function `list_scopes` outputs a list of scopes from the git commit history
    of the current directory, provided they adhere to the Angular commit
    convention: `type(scope)`.
    
    This new function can be used by running `list_scopes` in the command line.

--- zsh/cli.zsh
+++ zsh/cli.zsh
@@ -27,3 +27,37 @@ list_commits() {
     popd > /dev/null 2>&1;' sh "$1" \;
 }
 
+# ------------------------------------------
+# Function: list_scopes
+# Description:
+#     This function parses the git commit history of the current directory,
+#     searching for commit messages that follow the Angular commit convention.
+#     It extracts and prints the scopes from these commits. If 10 consecutive
+#     commits are found that do not follow the Angular convention, the function
+#     prints a warning message and reports the last known good commit.
+#
+# Usage:
+#     list_scopes
+# ------------------------------------------
+function list_scopes() {
+  local BAD_COUNT=0
+  local MAX_BAD_COUNT=10
+  local LAST_GOOD_COMMIT=""
+
+  git log --oneline | while read commit; do
+    if [[ $commit =~ "^([a-f0-9]+) ([a-z]+)\(([a-z]+)\):" ]]; then
+      echo "${match[3]}"
+      LAST_GOOD_COMMIT=$commit
+      BAD_COUNT=0
+    else
+      BAD_COUNT=$((BAD_COUNT + 1))
+      if [ $BAD_COUNT -ge $MAX_BAD_COUNT ]; then
+        echo "\nProbably only bad commits from here on out..."
+        if [ -n "$LAST_GOOD_COMMIT" ]; then
+          echo "Last detected good commit: $LAST_GOOD_COMMIT"
+        fi
+        return 1
+      fi
+    fi
+  done
+}
