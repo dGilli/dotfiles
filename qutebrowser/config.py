573ad713d6e330766d0b9310aeded29bdde7cdf7
Dennis Gilli <dennis.gilli@acribis.ch>
2023-12-16 12:39:41 +0100

    feat(qutebrowser) add qutebrowser preferences

---
+++ qutebrowser/config.py
@@ -0,0 +1,9 @@
+config.load_autoconfig()
+
+config.set("window.hide_decoration", True)
+config.set("colors.webpage.darkmode.enabled", True)
+
+config.set("colors.tabs.even.bg", '#000000')
+config.set("colors.tabs.odd.bg", '#000000')
+config.set("colors.tabs.selected.even.bg", '#3a3a3a')
+config.set("colors.tabs.selected.odd.bg", '#3a3a3a')
