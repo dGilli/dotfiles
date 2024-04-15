2a58ba63615095fdd16befc950aafe9e1096785e
Dennis Gilli <dennis.gilli@acribis.ch>
2024-04-15 11:32:44 +0200

    feat(nvim): clear old config and copy kickstart

--- nvim/after/plugin/fugitive.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gl", function()
-  vim.cmd("Git log --oneline")
-end)
--- nvim/lua/deg/init.lua
+++
@@ -1,2 +0,0 @@
-require("deg.remap")
-require("deg.set")
