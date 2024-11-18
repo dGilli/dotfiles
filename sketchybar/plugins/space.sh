a1abd53bfa026541e7019099fbf38a89cb5e98ec
dGilli <me@dennisgilli.com>
2024-11-18 09:48:25 +0100

    feat(menubar): improve sketchybar configurations
    - Adjust bar height to `33` and set color to black for better notch coverage.
    - Add Y offset to center the menu visually.
    - Change font weight to regular.
    - Remove space-switching behavior on click.
    - Resize front app chevron icon.
    - Tidy up some whitespace.

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -4,4 +4,9 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+if [ "$SELECTED" = "true" ]; then
+    sketchybar --set "$NAME" icon.color=0xfffff9b0
+else
+    sketchybar --set "$NAME" icon.color=0xff458588
+fi
+
